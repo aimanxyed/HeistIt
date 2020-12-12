cad8d2d0be37a955af37849650a1ea10ff2e944c
aimanmehtab18@gmail.com <Progamer@987>
2020-12-12 06:32:31 -0700

    fourth

--- app/src/main/java/com/unita/heistit/adapters/order_temp_adapter.kt
+++ app/src/main/java/com/unita/heistit/adapters/order_temp_adapter.kt
@@ -1,4 +1,24 @@
 package com.unita.heistit.adapters
 
-class order_temp_adapter {
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.unita.heistit.models.get_items
+import com.unita.heistit.models.get_order_temp
+
+class order_temp_adapter(var context: Context, var list: ArrayList<get_order_temp>) : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
---
+++ app/src/main/java/com/unita/heistit/models/get_order_temp.kt
@@ -0,0 +1,24 @@
+package com.unita.heistit.models
+
+class get_order_temp {
+    var id:Int
+    var name:String
+    var menus_name:String
+    var price:Double
+    var quantity:Int
+    var mobile:Int
+
+    constructor(id:Int, name:String , menus_name:String , price:Double , quantity:Int, mobile:Int)
+    {
+        this.id=id
+        this.name=name
+        this.menus_name=menus_name
+        this.price=price
+        this.quantity=quantity
+        this.mobile=mobile
+
+    }
+
+
+
+}
