872be52d0183ff95b9cbc0dc09e9ad4465a71454
aimanmehtab18@gmail.com <Progamer@987>
2020-12-14 06:09:22 -0700

    fivth

--- app/src/main/java/com/unita/heistit/SplashActivity.kt
+++ app/src/main/java/com/unita/heistit/SplashActivity.kt
@@ -6,16 +6,22 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class SplashActivity : AppCompatActivity() {
     private val splash_time_out:Long=3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+       // window.setFlags(
+        //  WindowManager.LayoutParams.FLAG_FULLSCREEN,
+          //  WindowManager.LayoutParams.FLAG_FULLSCREEN
+      //  )
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.pink))
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
 
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
 
